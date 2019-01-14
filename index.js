a604fc2e6c4f1e8df7d15f4bebfd8ee182745e67
Tuomas Siipola <siiptuo@kapsi.fi>
2019-01-14 20:00:55 +0200

    Support other color syntaxes

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ function findColors(node) {
   if (node.type == "color") {
     return [node.content];
   }
+  if (
+    node.type == "function" &&
+    ["rgb", "rgba", "hsl", "hsla"].includes(node.content[0].content)
+  ) {
+    return [node.toString()];
+  }
   if (Array.isArray(node.content)) {
     return node.content.map(findColors).flat();
   }
--- test.js
+++ test.js
@@ -4,8 +4,35 @@ const gonzales = require("gonzales-pe");
 const { findColors } = require("./");
 
 describe("findColors", () => {
-  it("should find color", () => {
-    const parseTree = gonzales.parse("body { color: #aabbcc }");
-    assert.deepEqual(findColors(parseTree), ["aabbcc"]);
+  it("should find #rrggbb colors", () => {
+    const parseTree = gonzales.parse("body { color: #ff0099 }");
+    assert.deepEqual(findColors(parseTree), ["ff0099"]);
+  });
+
+  it("should find #rrggbbaa colors", () => {
+    const parseTree = gonzales.parse("body { color: #ff0099ff }");
+    assert.deepEqual(findColors(parseTree), ["ff0099ff"]);
+  });
+
+  it("should find rgb() colors", () => {
+    const parseTree = gonzales.parse("body { color: rgb(255, 0, 153) }");
+    assert.deepEqual(findColors(parseTree), ["rgb(255, 0, 153)"]);
+  });
+
+  it("should find rgba() colors", () => {
+    const parseTree = gonzales.parse("body { color: rgba(51, 170, 51, .1) }");
+    assert.deepEqual(findColors(parseTree), ["rgba(51, 170, 51, .1)"]);
+  });
+
+  it("should find hsl() colors", () => {
+    const parseTree = gonzales.parse("body { color: hsl(270, 60%, 70%) }");
+    assert.deepEqual(findColors(parseTree), ["hsl(270, 60%, 70%)"]);
+  });
+
+  it("should find hsla() colors", () => {
+    const parseTree = gonzales.parse(
+      "body { color: hsla(240, 100%, 50%, .05) }"
+    );
+    assert.deepEqual(findColors(parseTree), ["hsla(240, 100%, 50%, .05)"]);
   });
 });
